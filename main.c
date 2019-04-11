26a348ea6ce938bd782cc4f23d61b6e8e59dcdaf
Matt C <matthew.cramp@uon.edu.au>
2019-04-11 05:24:01 +0000

    added print f

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
-    test 4-4-19
-    trial 2 /5
-    trial 3
-    return 0;
-    
-    test 11-4-19
+// this is the file for my assesment. dont mess with it. 
+
+int main() {
+
+ char alphabet [26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'};
+ char message [100];
+ int key;
+ int operation;
+ 
+printf("1. Encryption of a message with a rotation cipher given the message text and rotation amount\n");
+printf("2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount\n");
+printf("3. Encryption of a message with a substitution cipher given message text and alphabet substitution\n");
+printf("4. Decryption of a message encrypted with a substitution cipher given cipher text and substitutions\n");
+
+
+
+
+
+
+
+ return 0;
 }
+
